15a67c128205cede4290dbd31d7e1c64875cbcb8
ahawker <andrew.r.hawker@gmail.com>
2013-02-03 10:41:23 -0800

    Shifted globals into proper classes. Keywords init correctly (6 fields). CronExpression now takes in **kwargs and supports 'expr' for a cron string.

--- crython/crython.py
+++ crython/crython.py
@@ -8,16 +8,16 @@ import collections
 __author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
 __all__ = ['sec', 'min', 'hr', 'dom', 'mon', 'dow', 'yr']
 
-SPECIALS = {'*', '/', '%', ',', '-', 'L', 'W', '#', '?'}
-
 DAY_NAME = dict((v.lower(),k) for k,v in enumerate(calendar.day_name))      #(ex: Monday, Tuesday, etc)
 DAY_ABBR = dict((v.lower(),k) for k,v in enumerate(calendar.day_abbr))      #(ex: Mon, Tue, etc)
 MON_NAME = dict((v.lower(),k) for k,v in enumerate(calendar.month_name))    #(ex: January, February, etc)
 MON_ABBR = dict((v.lower(),k) for k,v in enumerate(calendar.month_abbr))    #(ex: Jan, Feb, etc)
 PHRASES  = dict(DAY_NAME.items() + DAY_ABBR.items() + MON_NAME.items() + MON_ABBR.items())
-PHRASES_REGEX = re.compile('{0}'.format('|'.join(PHRASES.keys()).lstrip('|')), flags=re.IGNORECASE)
+PHRASES_REGEX = re.compile('|'.join(PHRASES.keys()).lstrip('|'), flags=re.IGNORECASE)
 
 class CronField(collections.namedtuple('CronField', 'value min max specials')):
+    SPECIALS = {'*', '/', '%', ',', '-', 'L', 'W', '#', '?'}
+
     def __new__(cls, *args, **kwargs):
         """
         Creates a new CronField from the given time by substituting english dow/mon phrases for their
@@ -26,10 +26,15 @@ class CronField(collections.namedtuple('CronField', 'value min max specials')):
         args = (CronField.sub_english_phrases(args[0]),) + args[1:]
         field = super(CronField, cls).__new__(cls, *args, **kwargs)
         if isinstance(field.value, basestring):
-            invalid_chars = SPECIALS.difference(field.specials).intersection(set(field.value))
+            invalid_chars = cls.SPECIALS.difference(field.specials).intersection(set(field.value))
             if invalid_chars:
                 raise ValueError('Found invalid characters: {0}'.format(','.join(invalid_chars)))
         return field
+
+    def __repr__(self):
+        return '<CronField: {0}>'.format(self)
+    def __str__(self):
+        return self.value
 
     def __contains__(self, item):
         """
@@ -40,7 +45,7 @@ class CronField(collections.namedtuple('CronField', 'value min max specials')):
             return value == item
         if isinstance(value, basestring):
             result = False
-            for value in re.split(r'[,]', value):                   #comma separated values (ex: 1,4,10)
+            for value in value.split(','):                          #comma separated values (ex: 1,4,10)
                 value = re.split(r'[-/]', value)                    #separate range and step characters
                 if len(value) == 1:
                     if value[0] == '*':                             #single wildcard (ex: *)
@@ -67,26 +72,28 @@ class CronField(collections.namedtuple('CronField', 'value min max specials')):
         return PHRASES_REGEX.sub(_repl, value)
 
 #funcs for field creation so we don't need to subclass CronField with defaults
-sec = lambda *args,**kwargs: CronField(*(args + (0, 59,      {'*', '/', ',', '-'})),                **kwargs)
-min = lambda *args,**kwargs: CronField(*(args + (0, 59,      {'*', '/', ',', '-'})),                **kwargs)
-hr  = lambda *args,**kwargs: CronField(*(args + (0, 23,      {'*', '/', ',', '-'})),                **kwargs)
-dom = lambda *args,**kwargs: CronField(*(args + (1, 31,      {'*', '/', ',', '-', '?', 'L', 'W'})), **kwargs)
-mon = lambda *args,**kwargs: CronField(*(args + (1, 12,      {'*', '/', ',', '-'})),                **kwargs)
-dow = lambda *args,**kwargs: CronField(*(args + (0, 6,       {'*', '/', '-', '?', 'L', '#'})),      **kwargs)
-yr  = lambda *args,**kwargs: CronField(*(args + (1970, 2099, {'*', '/', ',', '-'})),                **kwargs)
+sec = lambda *args, **kwargs: CronField(*(args + (0, 59,      {'*', '/', ',', '-'})),                **kwargs)
+min = lambda *args, **kwargs: CronField(*(args + (0, 59,      {'*', '/', ',', '-'})),                **kwargs)
+hr  = lambda *args, **kwargs: CronField(*(args + (0, 23,      {'*', '/', ',', '-'})),                **kwargs)
+dom = lambda *args, **kwargs: CronField(*(args + (1, 31,      {'*', '/', ',', '-', '?', 'L', 'W'})), **kwargs)
+mon = lambda *args, **kwargs: CronField(*(args + (1, 12,      {'*', '/', ',', '-'})),                **kwargs)
+dow = lambda *args, **kwargs: CronField(*(args + (0, 6,       {'*', '/', '-', '?', 'L', '#'})),      **kwargs)
+yr  = lambda *args, **kwargs: CronField(*(args + (1970, 2099, {'*', '/', ',', '-'})),                **kwargs)
 
 
 CronTime = collections.namedtuple('CronTime', 'year month day hour minute second weekday')
 
 class CronExpression(object):
-    def __init__(self, second=None, minute=None, hour=None, day=None, month=None, weekday=None, year=None):
-        self.second  = sec(second or '*')
-        self.minute  = min(minute or '*')
-        self.hour    = hr(hour or '*')
-        self.day     = dom(day or '*')
-        self.month   = mon(month or '*')
-        self.weekday = dow(weekday or '*')
-        self.year    = yr(year or '*')
+    FIELDS = ('second', 'minute', 'hour', 'day', 'month', 'weekday')
+
+    def __init__(self, **kwargs):
+        for k,v in dict(zip(self.FIELDS, kwargs.get('expr', '* * * * * *').split(' ')), **kwargs).items():
+            setattr(self, k, v)
+
+    def __repr__(self):
+        return '<CronExpression: {0}>'.format(self)
+    def __str__(self):
+        return ' '.join(self.__dict__.values())
 
     def __contains__(self, item): #item should always be a datetime #XXX confirm and revise
         if not isinstance(item, datetime.datetime): #hrm
@@ -94,16 +101,14 @@ class CronExpression(object):
         item = CronTime(*item.timetuple()[:7])
         return all(getattr(item, k) in v for k,v in self.__dict__.items())
 
-    @staticmethod
-    def from_str(expr):
-        return CronExpression(expr.split(' '))
 
-KEYWORDS = {'yearly'   : CronExpression.from_str('0 0 1 1 *'),
-            'annually' : CronExpression.from_str('0 0 1 1 *'),
-            'monthly'  : CronExpression.from_str('0 0 1 * *'),
-            'weekly'   : CronExpression.from_str('0 0 * * 0'),
-            'daily'    : CronExpression.from_str('0 0 * * *'),
-            'hourly'   : CronExpression.from_str('0 * * * *'),
+KEYWORDS = {'yearly'   : CronExpression(expr='0 0 0 1 1 *'),
+            'annually' : CronExpression(expr='0 0 0 1 1 *'),
+            'monthly'  : CronExpression(expr='0 0 0 1 * *'),
+            'weekly'   : CronExpression(expr='0 0 0 * * 0'),
+            'daily'    : CronExpression(expr='0 0 0 * * *'),
+            'hourly'   : CronExpression(expr='0 0 * * * *'),
+            'minutely' : CronExpression(expr='0 * * * * *'),
             'reboot'   : None} #@reboot
 
 class CronJob(object):
@@ -114,4 +119,8 @@ class CronJob(object):
         self.kwargs = kwargs or {}
 
 class CronTab(threading.Thread):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(CronTab, self).__init__(*args, **kwargs)
+
+    def run(self):
+        pass
