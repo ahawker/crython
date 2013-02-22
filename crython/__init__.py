867442cbe6e308f9eca849d5e4709dce140f0350
ahawker <andrew.r.hawker@gmail.com>
2013-02-21 17:22:59 -0800

    Added name and version to __init__.py

--- crython/__init__.py
+++ crython/__init__.py
@@ -1,3 +1,5 @@
-__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
+__name__    = 'crython'
+__version__ = '0.0.1'
+__author__  = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
 
-from crython import *
+from crython import job, tab
