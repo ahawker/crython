ff9264842718c86f9a698ca989a7fb2636ab94cc
ahawker <andrew.r.hawker@gmail.com>
2013-02-21 17:21:49 -0800

    Removed __main__ and _version as they were no longer necessary.

--- crython/__main__.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
--- crython/_version.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
-__version__ = '0.0.1'
