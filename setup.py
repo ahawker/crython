145fe9f5c153dda641a029c3127571ac36f16dbe
ahawker <andrew.r.hawker@gmail.com>
2013-02-21 17:23:24 -0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+__author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
+
+import crython
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name             = crython.__name__,
+    version          = crython.__version__,
+    description      = 'Cron scheduling for python functions.',
+    long_description = open('README.md').read(),
+    author           = 'Andrew Hawker',
+    author_email     = 'andrew.r.hawker@gmail.com',
+    url              = 'https://github.com/ahawker/crython',
+    license          = open('LICENSE.md').read(),
+    package_dir      = {'crython': 'crython'},
+    packages         = ['crython'],
+    classifiers      = (
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+    )
+)
