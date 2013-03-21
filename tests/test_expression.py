ae3fa5980414ff786c58a2d0c3c336e51657dc17
ahawker <andrew.r.hawker@gmail.com>
2013-03-20 22:18:24 -0700

    Starting to add CronExpression level tests.

--- tests/test_expression.py
+++ tests/test_expression.py
@@ -1,10 +1,43 @@
 __author__ = 'Andrew Hawker <andrew.r.hawker@gmail.com>'
 
-import crython
+from crython.crython import CronExpression
 import unittest
 
 class TestCronExpression(unittest.TestCase):
-    pass
+    ATTRIBUTES = 'second minute hour day month weekday year'.split()
+
+    @classmethod
+    def has_all_attributes(cls, expr):
+        return all(k in expr.__dict__ for k in cls.ATTRIBUTES)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.default = CronExpression()
+        cls.wildcard = CronExpression(expr='* * * * * * *')
+
+        def keywords():
+            for k,v in CronExpression.KEYWORDS.items():
+                yield k.strip('@'), CronExpression(expr=v)
+        cls.keywords = dict(keywords())
+
+    def test_keywords(self):
+        for k,v in self.keywords.items():
+            assert CronExpression(expr='@{0}'.format(k)) == v
+
+    def test_attributes(self):
+        assert self.has_all_attributes(self.default)
+        assert self.has_all_attributes(self.wildcard)
+        assert all(map(self.has_all_attributes, self.keywords.values()))
+
+    def test_init_empty(self):
+        assert self.default == self.wildcard
+
+    def test_init_expr(self):
+        pass
+
+    def test_wildcard_defaults(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
