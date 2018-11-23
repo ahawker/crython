1320b11feb538cd8e9e89eb7b10eed108474ad0b
Andrew Hawker <andrew.r.hawker@gmail.com>
2018-11-23 15:25:47 -0800

    Remove old py26 compatibility logging
    This code was creating loggers in py26+ versions that had the name
    'root.crython...' instead of just 'crython.'. Since py26 is no longer
    supported, it should be safe to remove this code. It's a backwards
    compatbility breaking change however, for anyone that had figured
    that out and used 'root.' in their logging configs.

--- crython/log.py
+++ crython/log.py
@@ -7,17 +7,11 @@
 # pylint: disable=global-statement
 import logging
 
-ROOT_LOGGER = None
+ROOT_LOGGER = logging.getLogger(__package__)
 
 
 def get_logger(name=None):
     """
     Get a logger instance relative to the crython package.
     """
-    global ROOT_LOGGER
-
-    if ROOT_LOGGER is None:
-        ROOT_LOGGER = logging.getLogger()
-
-    name = '.'.join((ROOT_LOGGER.name, name)) if name else None
-    return logging.getLogger(name)
+    return ROOT_LOGGER.getChild(name)
